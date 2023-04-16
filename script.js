a62ae52c168d5a55061fc913be06846fc5f177bd
KhusBhavsar <125118845+KhusBhavsar@users.noreply.github.com>
2023-04-16 17:03:39 +0530

    Deletion of some files

--- script.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('load', function() {
-    setTimeout(function() {
-      var infoElements = document.querySelectorAll('.info, .infoImg');
-      for (var i = 0; i < infoElements.length; i++) {
-        infoElements[i].classList.add('show');
-      }
-    }, 3000);
-  });
